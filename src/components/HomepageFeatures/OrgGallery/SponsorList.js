b1669acd31ec86f1a622f82638ffe5191ba8f77a
Sheargrub <sheargrub@gmail.com>
2026-02-11 13:12:57 -0600

    Refactor organization gallery

---
+++ src/components/HomepageFeatures/OrgGallery/SponsorList.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        name: "NOAA",
+        logo: "https://ciroh.ua.edu/wp-content/uploads/2023/04/noaa-emblem-rgb-2022-1-150x150.png",
+        link: "https://www.noaa.gov/",
+        width: "120",
+        height: "120",
+    },
+    {
+        name: "USGS",
+        logo: "https://ciroh.ua.edu/wp-content/uploads/2023/04/USGS_logo_green-1.png",
+        link: "https://www.usgs.gov/",
+        width: "200",
+        height: "80",
+    },
+    {
+        name: "2I2C",
+        logo: "https://ciroh.ua.edu/wp-content/uploads/2023/04/2i2c_logo-150x150.png",
+        link: "https://2i2c.org/",
+        width: "120",
+        height: "120",
+    },
+    {
+        name: "Lynker",
+        logo: "https://ciroh.ua.edu/wp-content/uploads/2023/04/Lynker-no-tag.png",
+        link: "https://lynker.com/",
+        width: "200",
+        height: "80",
+    },
+    {
+        name: "OWP",
+        logo: "https://portal.ciroh.org/assets/images/owp_logo-cf6a5647bff6e38e14b683fb5ba3299c.png",
+        link: "https://water.noaa.gov/",
+        width: "300",
+        height: "300",
+    },
+];
---
+++ src/components/HomepageFeatures/OrgGallery/index.js
@@ -0,0 +1,101 @@
+import styles from "../styles.module.css";
+import Link from '@docusaurus/Link';
+import SponsorList from "./SponsorList";
+import MemberList from "./MemberList";
+import PartnerList from "./PartnerList";
+import ParentsPanel from "./ParentsPanel";
+
+export default function OrgGallery() {
+    return (
+        <div className="tw-w-full tw-text-blue-800 dark:tw-text-white">
+          <div className={`${styles.logoBackground} tw-rounded-xl tw-p-6`}>
+
+            <div className="tw-container tw-mx-auto">
+
+              {/* Consortium Sponsors */}
+              <div className="tw-col tw-col--12 tw-mb-10">
+                <div className={`${styles.heading} tw-text-blue-800 dark:tw-text-white`}>
+                  Consortium Sponsors
+                </div>
+
+                <div className={`${styles.flexListContainer} tw-flex tw-flex-wrap tw-gap-4`}>
+                  {SponsorList.map((sponsor) => (
+                    <Link
+                      to={sponsor.link}
+                      key={sponsor.name}
+                      className={`${styles.sponsorwrapper} tw-bg-slate-100 dark:tw-bg-white tw-rounded-lg tw-p-4 tw-flex tw-items-center tw-justify-center tw-shadow-sm`}
+                    >
+                      <img
+                        className={styles.sponsorcontainer}
+                        src={sponsor.logo}
+                        alt={sponsor.name}
+                        width={sponsor.width}
+                        height={sponsor.height}
+                      />
+                    </Link>
+                  ))}
+                </div>
+              </div>
+
+              {/* Consortium Members & Partners */}
+              <div className="tw-row tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-8">
+
+                {/* Members */}
+                <div>
+                  <div className={`${styles.heading} tw-text-blue-800 dark:tw-text-white`}>
+                    Consortium Members
+                  </div>
+
+                  <div className={`${styles.flexListContainer} tw-flex tw-flex-wrap tw-gap-4`}>
+                    {MemberList.map((member) => (
+                      <Link
+                        to={member.link}
+                        key={member.name}
+                        className={`${styles.imagewrapper} tw-bg-slate-100 dark:tw-bg-white tw-rounded-lg tw-p-4 tw-flex tw-items-center tw-justify-center tw-shadow-sm`}
+                      >
+                        <img
+                          className={styles.imagecontainer}
+                          src={member.logo}
+                          alt={member.name}
+                          width={member.width}
+                          height={member.height}
+                        />
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+
+                {/* Partners */}
+                <div>
+                  <div className={`${styles.heading} tw-text-blue-800 dark:tw-text-white`}>
+                    Consortium Partners
+                  </div>
+
+                  <div className={`${styles.flexListContainer} tw-flex tw-flex-wrap tw-gap-4`}>
+                    {PartnerList.map((partner) => (
+                      <Link
+                        to={partner.link}
+                        key={partner.name}
+                        className={`${styles.imagewrapper} tw-bg-slate-100 dark:tw-bg-white tw-rounded-lg tw-p-4 tw-flex tw-items-center tw-justify-center tw-shadow-sm`}
+                      >
+                        <img
+                          className={styles.imagecontainer}
+                          src={partner.logo}
+                          alt={partner.name}
+                          width={partner.width}
+                          height={partner.height}
+                        />
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              {/* NOAA + AWI + CIROH Logos */}
+              <ParentsPanel />
+
+            </div>
+          </div>
+        </div>
+    )
+}
